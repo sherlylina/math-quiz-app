46477a412ff4c608c36d1b69838835cd1d9ab929
sherlylina <sherlynjelina@gmail.com>
2021-01-23 01:12:58 +0800

    finish

--- public/src/script/main.js
+++ public/src/script/main.js
@@ -1,5 +1,11 @@
 
-let dataSoal, totalSeconds = 0, clearTime, time = 3600
+let dataSoal, 
+    totalSeconds = 0, 
+    clearTime, 
+    time = 3600,  
+    count = 0,
+    no = new Audio('./src/sound/no.mp3'),
+    yes = new Audio('./src/sound/yes.mp3');
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -42,10 +48,10 @@ function mainQuestion(params){
                         ${dataSoal[i].pertanyaan}
                     </div>
                     <div class="w-choice">
-                        <span class="box-answer">${dataSoal[i].jawaban2[0]}</span>
-                        <span class="box-answer">${dataSoal[i].jawaban2[1]}</span>
-                        <span class="box-answer">${dataSoal[i].jawaban2[2]}</span>
-                        <span class="box-answer">${dataSoal[i].jawaban2[3]}</span>
+                        <span class="box-answer ans-able">${dataSoal[i].jawaban2[0]}</span>
+                        <span class="box-answer ans-able">${dataSoal[i].jawaban2[1]}</span>
+                        <span class="box-answer ans-able">${dataSoal[i].jawaban2[2]}</span>
+                        <span class="box-answer ans-able">${dataSoal[i].jawaban2[3]}</span>
                     </div>
                 </div>`);
             }
@@ -58,7 +64,7 @@ function mainQuestion(params){
                 <div class="question" id=${i}>
                     <div class="soal">
                         <label for="answer-${i}">${dataSoal[i].pertanyaan}</label>
-                        <div class="bg-kayu">
+                        <div class="essay-input">
                             <input type="number" placeholder="Write Your Answer Here" id="answer-${i}" name="answer-${i}">
                         </div>
                     </div>
@@ -80,35 +86,50 @@ function mainQuestion(params){
         }
     });
 }
-    
-let count = 0;
-$(document).on('click', '.box-answer', function(){ 
+
+$(document).on('click', '.ans-able', function(){ 
     let ans = $(this).text();
     let ansParent = $(this).parents('.question').attr('id');
     if(count != 3){
         if(ans == dataSoal[ansParent].jawaban){
-            console.log('bener');
             Swal.fire({
                 icon: 'success',
                 title: 'Benar'
             });
+            $(this).removeClass('ans-able');
+            yes.play();
             count = count + 1;
             if(count == 3){
                 $('.back-practice').removeClass('grayscale');
+                updateStatus();
             }
-
         }else{
-            console.log('salah');
             Swal.fire({
                 icon: 'error',
                 title: 'Salah'
             });
+            no.play();
         }
     }
 }); 
 
 $(document).on('click', '.back-practice', function(){ 
-    window.location.href = './menu.html';
+    if(count == 3){
+        window.location.href = './menu.html';
+        updateStatus();
+    }
+}); 
+
+
+// local storage
+function resetStatus(){
+    window.localStorage.setItem("addition", false);
+    window.localStorage.setItem("subtraction", false);
+    window.localStorage.setItem("multiplication", false);
+    window.localStorage.setItem("division", false);
+};
+
+function updateStatus(){
     if($('.main-practice').attr('id') == 'addition'){
         window.localStorage.setItem("addition", true);
     }
@@ -121,10 +142,7 @@ $(document).on('click', '.back-practice', function(){
     if($('.main-practice').attr('id') == 'division'){
         window.localStorage.setItem("division", true);
     }
-    console.log('a')
-}); 
-
-
+};
 
 $(document).on('click', '.w-num', function(){ 
     let ans = $(this).text();
@@ -144,6 +162,7 @@ $('#myForm').on('submit', function(e) {
     window.localStorage.setItem("result", JSON.stringify(values));
     window.location.href = './result.html';
 });
+
 
 function retrieveResult(){
     let i;
@@ -172,24 +191,17 @@ function retrieveResult(){
             // };
         };
     }
-
-    if(count == 0 || count <= 99){
-        console.log('a');
+    if(count >= 0 && count <= 99){
         $('.star img').attr('src', './src/images/comp/Button-Kayu-Star.png');
-    }else if(count >= 100 || count <= 199){
-        console.log('b');
+    }else if(count >= 100 && count <= 199){
         $('.star img').attr('src', './src/images/comp/Bintang-1.png');
-    }else if(count >= 200 || count <= 299){
-        console.log('c');
+    }else if(count >= 200 && count <= 299){
         $('.star img').attr('src', './src/images/comp/Bintang-2.png');
-    }else if(count >= 300){
-        console.log('d');
+    }else {
         $('.star img').attr('src', './src/images/comp/Bintang-3.png');
     }
-
-    
     $('.total').text('Your Score is ' + count);
-}
+};
 
 $('#myForm').on('input paste', 'input', function(){
     // Get all the forms elements and their values in one step
@@ -208,6 +220,24 @@ $('#myForm').on('input paste', 'input', function(){
     window.localStorage.setItem("result", JSON.stringify(values));
 });
 
+
+$(document).on('click', '.ask-back', function(){ 
+    Swal.fire({
+        title: 'Make your choice!',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Retry Exam',
+        cancelButtonText: 'Main Menu'
+        }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "./exam.html"
+        }else{  
+            window.location.href = "./menu.html";
+            resetStatus();
+        }
+    })
+}); 
 
 // timer;
 function setTime() {
@@ -228,31 +258,3 @@ function pad(val) {
     return valString;
   }
 }
-
-$(document).on('click', '.ask-back', function(){ 
-    Swal.fire({
-        title: 'Make your choice!',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Retry Exam',
-        cancelButtonText: 'Main Menu'
-        }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = "./exam.html"
-        }else{  
-            window.location.href = "./menu.html";
-        
-            window.localStorage.setItem("addition", false);
-    
-            window.localStorage.setItem("subtraction", false);
-        
-        
-            window.localStorage.setItem("multiplication", false);
-        
-
-            window.localStorage.setItem("division", false);
-            
-        }
-    })
-}); 
